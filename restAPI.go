45cdfe1ccd6c601871e28508cafa5717844d8988
ryanprtma <ryanapratama16@gmail.com>
2022-02-24 18:11:01 +0700

    tugas fetching restAPI

---
+++ restAPI.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
+
+}
